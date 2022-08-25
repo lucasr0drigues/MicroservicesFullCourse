189d3eed41770175722a25b329fe264d3da395d4
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-25 12:58:41 -0300

    add plataform model and db context

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -1,10 +1,20 @@
-namespace PlatformService.Models;
+using System.ComponentModel.DataAnnotations;
 
-public class PlatformService
+namespace PlatformService.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Publisher { get; set; }
-    public string Cost { get; set; }
-    
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Publisher { get; set; }
+
+        [Required]
+        public string Cost { get; set; }
+    }
 }
