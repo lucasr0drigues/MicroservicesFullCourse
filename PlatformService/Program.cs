8c08b2a80877360d831a316ed41ad6bbd7d7e8fa
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-25 13:52:40 -0300

    add platform dtos and mappings

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -7,6 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseInMemoryDatabase("InMem");
