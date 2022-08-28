0536aa493a83c459f70f2e9cfa027d5ed2299da9
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-28 19:09:51 -0300

    add HttpCommandDataClient

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("InMem");
 });
 builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
 
 var app = builder.Build();
 
