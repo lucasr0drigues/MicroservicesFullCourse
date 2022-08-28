9ab0cefc1de29ca5df14f56cc32a40fc741e23a5
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-28 01:52:36 -0300

    add platforms controller

--- CommandService/Controllers/PlatformsController.cs
+++ CommandService/Controllers/PlatformsController.cs
@@ -1,7 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace CommandService.Controllers
 {
+    [ApiController]
+    [Route("api/c/[controller]")]
     public class PlatformsController : ControllerBase
     {
+        public PlatformsController()
+        {
+            
+        }
 
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("---> Inbound POST # Command Service");
+            
+            return Ok("Inbound test from Platforms Controller");
+        }
     }
 }
