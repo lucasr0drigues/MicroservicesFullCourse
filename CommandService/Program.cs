b574943045684214033da5b6748f6b911d579114
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-08-29 13:00:44 -0300

    add automapper profile

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
